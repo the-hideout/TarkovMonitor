8bd81af69c6c9d3bd904abe884955287630b9ae6
Razzmatazz <razz@badkarma.xyz>
2023-03-02 09:40:34 -0600

    fix task failures

--- TarkovMonitor/MainBlazorUI.cs
+++ TarkovMonitor/MainBlazorUI.cs
@@ -162,7 +162,7 @@ namespace TarkovMonitor
                 {
                     if (failCondition.task.id == e.TaskId && failCondition.status.Contains("complete"))
                     {
-                        Eft_TaskFailed(sender, new GameWatcher.TaskEventArgs { TaskId = failCondition.task.id });
+                        Eft_TaskFailed(sender, new GameWatcher.TaskEventArgs { TaskId = task.id });
                     }
                 }
             });
