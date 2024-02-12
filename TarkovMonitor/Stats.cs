44c80cdbf7c7febadc4208cadc6c76ed998ff567
Razzmatazz <razz@badkarma.xyz>
2024-02-11 21:08:15 -0600

    debug

--- TarkovMonitor/Stats.cs
+++ TarkovMonitor/Stats.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SQLite;
-using System.Windows.Forms;
+﻿using System.Data.SQLite;
 
 namespace TarkovMonitor
 {
