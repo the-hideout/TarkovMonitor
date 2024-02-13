b6d219c0ca4915f0971a5431525bc36b15c4f26f
Razzmatazz <razz@badkarma.xyz>
2024-02-12 18:36:31 -0600

    path string in quotes?

--- TarkovMonitor/Stats.cs
+++ TarkovMonitor/Stats.cs
@@ -5,7 +5,7 @@ namespace TarkovMonitor
     internal class Stats
     {
         public static string DatabasePath => Path.Join(Application.UserAppDataPath, "TarkovMonitor.db");
-        public static string ConnectionString => $"Data Source={DatabasePath};Version=3;";
+        public static string ConnectionString => $"Data Source=\"{DatabasePath}\";Version=3;";
         private static SQLiteConnection? Connection;
         public static void OpenConnection()
         {
