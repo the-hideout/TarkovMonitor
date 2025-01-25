70742740e4f06200244558b37f437916dd609359
Wraiith32 <mailer@imaxa.com>
2025-01-24 20:14:29 -0700

    NormalizedName of object was always null. Traced back and noticed that normalizedName was not included in query. Air Filter Notification was never working. Should be fixed now.

--- TarkovMonitor/TarkovDev.cs
+++ TarkovMonitor/TarkovDev.cs
@@ -178,6 +178,7 @@ namespace TarkovMonitor
                         hideoutStations {
                             id
                             name
+                            normalizedName
                             levels {
                                 id
                                 level
